e75ac2608028025e6e2932452db9b7a49462ea29
vkmouse <knight31241@gmail.com>
2022-06-25 16:33:18 +0800

    modify DetectionResults

--- AutoMacro/Detection/Detection/DetectionResults.cpp
+++ AutoMacro/Detection/Detection/DetectionResults.cpp
@@ -1,51 +1,56 @@
 #include "AutoMacro/Detection/Detection/DetectionResults.h"
 
+#include <functional>
+#include <vector>
+
 #include "AutoMacro/Core/Core.h"
 #include "AutoMacro/Detection/Detection/DetectionResult.h"
 
 namespace AutoMacro {
 namespace Detection {
+namespace {
+bool exists(const std::vector<DetectionResult>* results,
+    std::function<bool(const DetectionResult&)> func) {
+    auto iterator = std::find_if(results->begin(), results->end(), func);
+    bool found = iterator != results->end();
+    return found;
+}
+
+DetectionResults where(const std::vector<DetectionResult>* results,
+    std::function<bool(const DetectionResult&)> func) {
+    DetectionResults output;
+    auto iterator = std::find_if(results->begin(), results->end(), func);
+    while (iterator != results->end()) {
+        output.push_back(*iterator);
+        iterator = std::find_if(iterator + 1, results->end(), func);
+    }
+    return output;
+}
+}  // namespace
+
 bool DetectionResults::exists(float threshold, int index) const {
     auto func = [&index, &threshold](const DetectionResult& item) {
         return item.index == index && item.confidence > threshold;
     };
-    return exists(func);
+    return AutoMacro::Detection::exists(this, func);
 }
 
 bool DetectionResults::exists(float threshold) const {
     auto func = [&threshold](const DetectionResult& item) {
         return item.confidence > threshold;
     };
-    return exists(func);
+    return AutoMacro::Detection::exists(this, func);
 }
 
 DetectionResults DetectionResults::where(float threshold) const {
     auto func = [&threshold](const DetectionResult& item) {
         return item.confidence > threshold;
     };
-    return where(func);
+    return AutoMacro::Detection::where(this, func);
 }
 
 int DetectionResults::count(float threshold) const {
     return static_cast<int>(where(threshold).size());
-}
-
-bool DetectionResults::exists(
-    std::function<bool(const DetectionResult&)> func) const {
-    auto iterator = std::find_if(begin(), end(), func);
-    bool found = iterator != end();
-    return found;
-}
-
-DetectionResults DetectionResults::where(
-    std::function<bool(const DetectionResult&)> func) const {
-    DetectionResults results;
-    auto iterator = std::find_if(begin(), end(), func);
-    while (iterator != end()) {
-        results.push_back(*iterator);
-        iterator = std::find_if(iterator + 1, end(), func);
-    }
-    return results;
 }
 }  // namespace Detection
 }  // namespace AutoMacro
--- AutoMacro/Detection/Detection/DetectionResults.h
+++ AutoMacro/Detection/Detection/DetectionResults.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <functional>
 #include <vector>
 
 #include "AutoMacro/Core/Core.h"
@@ -9,15 +8,12 @@ namespace AutoMacro {
 namespace Detection {
 class DetectionResults : public std::vector<DetectionResult> {
  public:
+    using vector::vector;
+
     DLL_EXPORTS bool exists(float threshold, int index) const;
     DLL_EXPORTS bool exists(float threshold) const;
     DLL_EXPORTS DetectionResults where(float threshold) const;
     DLL_EXPORTS int count(float threshold) const;
-
- private:
-    bool exists(std::function<bool(const DetectionResult&)> func) const;
-    DetectionResults where(
-        std::function<bool(const DetectionResult&)> func) const;
 };
 }  // namespace Detection
 }  // namespace AutoMacro
